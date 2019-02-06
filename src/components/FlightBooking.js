3db6935e3b0bec221bba5246578a3e0886baa26a
David Pearl <davidtpearl@gmail.com>
2019-02-06 22:35:43 +1100

    Got seats to display for a flight. Added some styling to seats.

--- src/components/FlightBooking.js
+++ src/components/FlightBooking.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import DisplaySeats from './DisplaySeats';
+
+const SERVER_URL_FLIGHTS = 'http://localhost:3001/flights.json';
+const SERVER_URL_FLIGHT = 'http://localhost:3001/flights/1.json'
 
 class FlightBooking extends Component {
   constructor() {
@@ -6,33 +11,37 @@ class FlightBooking extends Component {
     this.state = {
       // Information sent from Flight search
       // Flight ID, plane information
-      flightID: 1,
+      planeID: 1,
       seats: 12,
       // Information handled in FlightBooking
       seatsBooked: Array(12).fill('Available')
       // Array with null values (or 'available') for each seat on the airplane. Array(rows * cols).fill(null)
     };
-    this._displaySeats = this._displaySeats.bind(this);
   }
 
-  _displaySeats() {
-    // This method should display the seats on the airplane
-    console.log( this.state.seatsBooked );
 
-    // const fetchSecrets = () => {
-    //   axios.get(SERVER_URL).then( (results) => {
-    //     this.setState({secrets: results.data});
-    //     setTimeout(fetchSecrets, 4000);
-    //   });
-    // };
-    //
-    // fetchSecrets();
+
+  componentDidMount() {
+    axios.get(SERVER_URL_FLIGHT).then( (results) => {
+      const flightData = results.data;
+      console.log(flightData);
+      this.setState({
+        planeID: flightData.plane.id,
+        seats: (flightData.plane.row * flightData.plane.col),
+        seatsBooked: Array(flightData.plane.row * flightData.plane.col).fill('Available')
+       });
+    });
+
+    // Send seats info to DisplaySeats (results.data...)
   }
+
+
 
   render () {
     return(
       <div>
         <h2>Burning Airlines booking system is totally coming soon</h2>
+        <DisplaySeats seatsBooked={ this.state.seatsBooked }/>
       </div>
     );
   }
